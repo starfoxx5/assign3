91d2755c76633587ae0ed55ae61a86eeec0c8e04
Enrique Flores <ehf.81@hotmail.com>
2021-02-09 01:52:40 -0800

    change

--- js/ex3.js
+++ js/ex3.js
@@ -1,5 +1,5 @@
 /* Input calcuation */
-const num1 = Number(prompt('Enter a number:'));
-const num2 = Number(prompt('Enter another number:'));
+let num1 = Number(prompt('Enter a number:'));
+let num2 = Number(prompt('Enter another number:'));
 
 console.log('${num1} + ${num2} = ${num1 + num2}');
