90152d98928bf7bc77354dfb3cac1d2e48daaba5
Enrique Flores <ehf.81@hotmail.com>
2021-02-09 15:59:28 -0800

    edit

--- js/ex7.js
+++ js/ex7.js
@@ -1,6 +1,8 @@
-console.log(4 + 5);
-console.log("4 + 5");
-console.log("4" + "5");
-console.log(2 + 3 * 5);
-console.log((2 + 3) * 5);
-console.log(10 % 3);
+let num1 = 0;
+{
+  num1 = 1; 
+  const num2 = 0;
+}
+console.log(num1); 
+console.log(num2); 
+
