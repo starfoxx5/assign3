1a6a8107d5d4d3bea9ec6b4b4edb3b32a5e1b317
Enrique Flores <ehf.81@hotmail.com>
2021-02-09 00:39:30 -0800

    edit

--- js/ex1.js
+++ js/ex1.js
@@ -1,16 +1,16 @@
 /* Input name & number of units */ 
-const fname = String(prompt('Enter your first name:'));
+const fname = prompt('Enter your first name:');
 const units = Number(prompt('Enter the current number of completed units:'));
-if (units >=0 && units <31) {
-  alert('Hello ${fname}. Your grade standing is Freshman');
-}
-if (units >=31 && units <61) {
-  alert('Hello ${fname}. Your grade standing is Sophomore');
-}
-if (units >=61 && units <91) {
-  alert('Hello ${fname}. Your grade standing is Junior');
-}
-if (units >=91) {
-  alert('Hello ${fname}. Your grade standing is Senior');
-}
+  if (units >=0 && units <31) {
+  console.log('Hello ${fname}. Your grade standing is Freshman');
+  }
+  if (units >=31 && units <61) {
+  console.log('Hello ${fname}. Your grade standing is Sophomore');
+  }
+  if (units >=61 && units <91) {
+  console.log('Hello ${fname}. Your grade standing is Junior');
+  }
+  if (units >=91) {
+  console.log('Hello ${fname}. Your grade standing is Senior');
+  }
 
