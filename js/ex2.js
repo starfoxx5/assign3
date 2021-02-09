8390ca107348088e74bb92147036197c842e3afa
Enrique Flores <ehf.81@hotmail.com>
2021-02-09 01:26:14 -0800

    ex2

---
+++ js/ex2.js
@@ -0,0 +1,20 @@
+/* Input day of the week */ 
+const day = prompt('Enter the day of the week (ex. sun, mon, tue):');
+switch (day){
+  case "sun":
+    console.log("You entered: ${day}");
+    console.log("The following day is: mon");
+    break;
+  case "mon":
+    console.log("You entered: ${day}");
+    console.log("The following day is: tue");
+    break;
+
+
+
+
+
+
+
+
+}
