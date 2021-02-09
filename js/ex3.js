24ce49b6a7a702dba6dc1283a2a50552f680f8fe
Enrique Flores <ehf.81@hotmail.com>
2021-02-09 03:13:43 -0800

    edit

--- js/ex3.js
+++ js/ex3.js
@@ -2,8 +2,11 @@
 let num1 = Number(prompt('Enter a number:'));
 let num2 = Number(prompt('Enter another number:'));
 
-console.log('${num1} + ${num2} = ${num1 + num2}');
-console.log('${num1} - ${num2} = ${num1 - num2}');
-console.log('${num1} * ${num2} = ${num1 * num2}');
-console.log('${num1} / ${num2} = ${num1 / num2}');
-console.log('${num1} % ${num2} = ${num1 % num2}');
+console.log(`${num1} + ${num2} = ${num1 + num2}`);
+console.log(`${num1} - ${num2} = ${num1 - num2}`);
+console.log(`${num1} * ${num2} = ${num1 * num2}`);
+console.log(`${num1} / ${num2} = ${num1 / num2}`);
+console.log(`${num1} % ${num2} = ${num1 % num2}`);
+
+
+
