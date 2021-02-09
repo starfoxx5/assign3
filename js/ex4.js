c82a716fe10828152b4a18b5f07eba7333437baf
Enrique Flores <ehf.81@hotmail.com>
2021-02-09 04:05:33 -0800

    edit

--- js/ex4.js
+++ js/ex4.js
@@ -24,3 +24,28 @@ function isNumericUsage() {
       }
   }
 }
+
+<h2>Input Password and Submit [7 to 15 characters which contain only characters, numeric digits, underscore and first character must be a letter]</h2
+<form name="form1" action="#">
+<ul>
+<li><input type='text' name='text1'/></li>
+<li class="rq">*Enter numbers only.</li>
+<li>&nbsp;</li>
+<li class="submit"><input type="submit" name="submit" value="Submit" onclick="CheckPassword(document.form1.text1)"/></li>
+<li>&nbsp;</li>
+</ul>
+
+function CheckPassword(inputtxt) 
+{ 
+var passw=  /^[A-Za-z]\w{7,14}$/;
+if(inputtxt.value.match(passw)) 
+{ 
+alert('Correct, try another...')
+return true;
+}
+else
+{ 
+alert('Wrong...!')
+return false;
+}
+}
