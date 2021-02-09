49165b6483ea2e1c9599f601f5a761ba8b91e6df
Enrique Flores <ehf.81@hotmail.com>
2021-02-09 00:25:56 -0800

    name

--- js/ex1.js
+++ js/ex1.js
@@ -5,12 +5,12 @@ if (units >=0 && units <31) {
   console.log('${fname} Hello ${fname} . Your grade standing is Freshman');
 }
 if (units >=31 && units <61) {
-  alert('Hello ${name}. Your grade standing is Sophomore');
+  console.log('Hello ${fname}. Your grade standing is Sophomore');
 }
 if (units >=61 && units <91) {
-  alert('Hello ${name}. Your grade standing is Junior');
+  console.log('Hello ${fname}. Your grade standing is Junior');
 }
 if (units <=91) {
-  alert('Hello ${name}. Your grade standing is Senior');
+  alert('Hello ${fname}. Your grade standing is Senior');
 }
-${name}
+
