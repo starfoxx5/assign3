06791e8b8868c4c3261d5e086adc89ccdda0a83c
Enrique Flores <ehf.81@hotmail.com>
2021-02-09 03:21:31 -0800

    edit

--- js/ex2.js
+++ js/ex2.js
@@ -2,31 +2,31 @@
 const day = prompt('Enter the day of the week (ex. sun, mon, tue):');
 switch (day){
   case "sun":
-    console.log("You entered: ${day}");
+    console.log(`You entered: ${day}`);
     console.log("The following day is: mon");
     break;
   case "mon":
-    console.log("You entered: ${day}");
+    console.log(`You entered: ${day}`);
     console.log("The following day is: tue");
     break;
   case "tue":
-    console.log("You entered: ${day}");
+    console.log(`You entered: ${day}`);
     console.log("The following day is: wed");
     break;
   case "wed":
-    console.log("You entered: ${day}");
+    console.log(`You entered: ${day}`);
     console.log("The following day is: thu");
     break;
   case "thu":
-    console.log("You entered: ${day}");
+    console.log(`You entered: ${day}`);
     console.log("The following day is: fri");
     break;
   case "fri":
-    console.log("You entered: ${day}");
+    cconsole.log(`You entered: ${day}`);
     console.log("The following day is: sat");
     break;
   case "sat":
-    console.log("You entered: ${day}");
+    console.log(`You entered: ${day}`);
     console.log("The following day is: sun");
     break;
 }
