ca0ad0e0f486aad4577efff1b5c7b426c929be31
Enrique Flores <ehf.81@hotmail.com>
2021-02-08 23:57:26 -0800

    ex1

--- js/ex1.js
+++ js/ex1.js
@@ -1,2 +1,15 @@
-/* Tutorial 4 Example 1 JavaScript code */ 
-console.log("Hello from E");
+/* Input name & number of units */ 
+const name = prompt('Enter your first name:');
+const units = Number(prompt('Enter your current number of units completed:'));
+if (units <31) {
+  alert('Hello ${name}. Your grade standing is Freshman')
+}
+if (units >=31 && units <61) {
+  alert('Hello ${name}. Your grade standing is Sophomore')
+}
+if (units >=61 && units <91) {
+  alert('Hello ${name}. Your grade standing is Junior')
+}
+else {
+  alert('Hello ${name}. Your grade standing is Senior')
+}
