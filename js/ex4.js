78b18eca047a0b1346a6d0689ad27cfa9b375707
Enrique Flores <ehf.81@hotmail.com>
2021-02-09 17:30:55 -0800

    edit

--- js/ex4.js
+++ js/ex4.js
@@ -1,51 +1,14 @@
-/* Here's how to use the isNumeric() function. */
-function isNumericUsage() {
-  "use strict";
+function promptPassword( )
+{
 
-  // Declare Constants
-  const INITIAL_VALUE = 0;
-  const LOOP_LIMIT = 3;
+var name = prompt ("Enter your Username: ");
+var pwd = prompt ("Enter your Password: ");
 
-  // Declare Variables
-  let entry;
-  let counter;
-
-  // Ask a user to enter five things
-  for (counter = INITIAL_VALUE; counter < LOOP_LIMIT; counter++) {
-      // Get a number or a string
-      entry = prompt("Enter Password");
-
-      if ((entry)) {
-          // In here what was entered IS a number.
-          document.write("You entered a number: " + entry + ".<br />");
-      } else {
-          // In here what was entered is a not numeric.
-          document.write("You entered a string: " + entry + ".<br />");
-      }
-  }
+while (pwd != 'P@ssw0rd'){
+alert("Login is incorrect");
+pwd = prompt ("Enter your Password: ");
 }
 
-<h2>Input Password and Submit [7 to 15 characters which contain only characters, numeric digits, underscore and first character must be a letter]</h2
-<form name="form1" action="#">
-<ul>
-<li><input type='text' name='text1'/></li>
-<li class="rq">*Enter numbers only.</li>
-<li>&nbsp;</li>
-<li class="submit"><input type="submit" name="submit" value="Submit" onclick="CheckPassword(document.form1.text1)"/></li>
-<li>&nbsp;</li>
-</ul>
+alert("Password is correct, you are allowed to enter the site");
 
-function CheckPassword(inputtxt) 
-{ 
-var passw=  /^[A-Za-z]\w{7,14}$/;
-if(inputtxt.value.match(passw)) 
-{ 
-alert('Correct, try another...')
-return true;
-}
-else
-{ 
-alert('Wrong...!')
-return false;
-}
 }
