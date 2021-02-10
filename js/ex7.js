b3d94f04bb8b8afb83a1074844a31501807c04ac
Enrique Flores <ehf.81@hotmail.com>
2021-02-09 17:11:24 -0800

    edit

--- js/ex7.js
+++ js/ex7.js
@@ -1,14 +1,16 @@
-let x = 3;
+let x = 2;
 let y = 3;
 
 if (x > 2) {
   if  (y > 2) {
-     z = x + y;
-     console.log("z is: ", z);
+  z = x + y;
+  console.log("z is: ", z);
   }
-} else {
+  else {
   console.log("x is: ", x);
+  }
 }
+  
  
 
 
