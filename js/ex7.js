3ada496aff34a48ab169b52c30c69a925191e3aa
Enrique Flores <ehf.81@hotmail.com>
2021-02-09 16:10:50 -0800

    edit

--- js/ex7.js
+++ js/ex7.js
@@ -1,5 +1,14 @@
-let pi = 3.14;
-pi = 3.14159;
-console.log(pi);
+let a = 2;
+a -= 1;
+a++;
+let b = 8;
+b += 2;
+const c = a + b * b;
+const d = a * b + b;
+const e = a * (b + b);
+const f = a * b / a;
+const g = b / a * a;
+console.log(a, b, c, d, e, f, g);
+
 
 
