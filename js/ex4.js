43f9ab397ac5633314f68c66137467ed5cf250fe
Enrique Flores <ehf.81@hotmail.com>
2021-02-09 17:59:51 -0800

    edit

--- js/ex4.js
+++ js/ex4.js
@@ -1,25 +1,4 @@
-var password = "wordpass";
-var entryCount = 0;
-var entryLimit = 3;
 
-button.addEventListener("click", function(){
-  if (entryCount < entryLimit) {
-    if (textbox.value != password){
-      answer.innerHTML = "Wrong Password";
-      entryCount++;
-      textbox.value = "";
-    } else {
-      // success!
-      entryCount = 0;
-      answer.innerHTML = "Correct Password";
-    }
-  } else {
-      answer.innerHTML = "Out of entries";
-  }
-});
-
-
-/*
 const pwd = 'secret'
 let number = 0
 
@@ -27,7 +6,7 @@ function promptPassword( )
 {
     prompt ("Enter your Password: ");
     
-    while (pwd != 'secret'){
+    while (pwd === 'secret'){
         alert("Login is incorrect");
         prompt ("Enter your Password: ");
     }
@@ -35,4 +14,4 @@ function promptPassword( )
     alert("Password is correct, you are allowed to enter the site");
     
 }
-promptPassword(); */
+promptPassword(); 
