66edc3eff33ff5322805a67f00249e44ba9618d4
Enrique Flores <ehf.81@hotmail.com>
2021-02-09 17:33:50 -0800

    es

--- js/ex4.js
+++ js/ex4.js
@@ -1,14 +1,15 @@
 function promptPassword( )
 {
-
-var name = prompt ("Enter your Username: ");
-var pwd = prompt ("Enter your Password: ");
-
-while (pwd != 'P@ssw0rd'){
-alert("Login is incorrect");
-pwd = prompt ("Enter your Password: ");
+    
+    var name = prompt ("Enter your Username: ");
+    var pwd = prompt ("Enter your Password: ");
+    
+    while (pwd != 'P@ssw0rd'){
+        alert("Login is incorrect");
+        pwd = prompt ("Enter your Password: ");
+    }
+    
+    alert("Password is correct, you are allowed to enter the site");
+    
 }
-
-alert("Password is correct, you are allowed to enter the site");
-
-}
+promptPassword();
